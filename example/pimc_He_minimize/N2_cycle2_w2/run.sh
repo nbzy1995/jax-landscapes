5d3891d0116774a3b73a4d4e826468818e1445bc
yang <nbzy1995@gmail.com>
2025-11-24 22:18:37 -0500

    add N2 cycle2-w2 example

---
+++ example/pimc_He_minimize/N2_cycle2_w2/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run PIMC minimization for N2_cycle2_w2 example
+#
+# System: N=2 Helium atoms, 2-cycle (exchange), winding number 2
+
+cd ..
+
+python minimize_pimc.py \
+    N2_cycle2_w2/input \
+    N2_cycle2_w2/output \
+    --N 2 \
+    --box-size 20.0 \
+    --T 1.0 \
+    --mass 4.0026 \
+    --hbar 7.638 \
+    --configs 0 \
+    --save-every 10 \
+    --maxiter 5000 \
+    --escape-saddles
+
